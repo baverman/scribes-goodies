16822b113815369231378707e5d742847931a0c2
Anton Bobrov <bobrov@vl.ru>
2010-09-18 19:58:08 +1100

    Ability to save shortcut configuration

--- plugins/ShortcutManager/scribes/editor/shortcut_manager/__init__.py
+++ plugins/ShortcutManager/scribes/editor/shortcut_manager/__init__.py
@@ -1,19 +1,19 @@
 import gc
+import os.path
 
 from gtk import accel_groups_from_object, accelerator_parse, accelerator_name
-from gtk import ACCEL_LOCKED, STOCK_PREFERENCES
+from gtk import ACCEL_LOCKED, STOCK_PREFERENCES, keysyms
 from pango import WEIGHT_NORMAL, WEIGHT_BOLD
 
 from SCRIBES.Trigger import Trigger
 from SCRIBES.TriggerManager import TriggerManager
 from SCRIBES.TriggerSystem.AcceleratorActivator import Activator
+from SCRIBES.Utils import open_database
 
 from scribes.helpers import connect_external, connect_all
 
 old_create_trigger = None
-shortcuts = {
-    'show-quick-open-window': '<ctrl><alt>r'
-}
+shortcuts = {}
 
 def new_create_trigger(self, name, accelerator="", description="", category="", error=True, removable=True):
     if name in shortcuts:
@@ -27,21 +27,19 @@ def find_objects_of(cls):
         if isinstance(r, cls):
             yield r
 
-def replace_accelerator(old_accel, new_accel, trigger=None):
-    old_accel = accelerator_parse(old_accel)
-    for a in find_objects_of(Activator):
-        if old_accel in a._Activator__dictionary:
-            trigger = a._Activator__dictionary[old_accel]
-            a._Activator__remove(trigger)
-
-            trigger._Trigger__accelerator = new_accel
-    
-            print 'Replaced ', trigger.name, trigger.accelerator
-            a._Activator__add(trigger)
-        else:
-            trigger._Trigger__accelerator = new_accel
-            print 'too fast ', trigger.name, trigger.accelerator
-
+def replace_accelerator(name, new_accel, startup):
+    activators = dict((a._Activator__editor, a) for a in find_objects_of(Activator))
+    for t in find_objects_of(Trigger):
+        if t.name == name:
+            a = activators[t._Trigger__editor]
+            
+            trigger_in = t in a._Activator__dictionary.values()
+            
+            a._Activator__remove(t)
+            t._Trigger__accelerator = new_accel
+            
+            if not startup or trigger_in:
+                a._Activator__add(t)
 
 class Plugin(object):
     already_loaded = False
@@ -53,7 +51,7 @@ class Plugin(object):
     def replace_existing_triggers(self):
         for t in find_objects_of(Trigger):
             if t.name in shortcuts:
-                replace_accelerator(t.accelerator, shortcuts[t.name], t)
+                replace_accelerator(t.name, shortcuts[t.name], True)
 
     def patch_trigger_manager(self):
         global old_create_trigger
@@ -84,10 +82,12 @@ class Plugin(object):
         self.actions.clear()
         
         category = {}
+        sortkey = lambda v: (v.category if v.category else 'Without category') + v.name
         
-        for t in sorted(self.editor.triggers, key=lambda v: (v.category if v.category else 'Without category') + v.name):
+        for t in sorted(self.editor.triggers, key=sortkey):
             if not t.category in category:
-                category[t.category] = self.actions.append(None, (t.category, None, None, False, 400))
+                category[t.category] = self.actions.append(
+                    None, (t.category, None, None, False, WEIGHT_NORMAL))
             
             self.actions.append(category[t.category], (t.name, t.accelerator, t.description,
                 True, WEIGHT_BOLD if t.name in shortcuts else WEIGHT_NORMAL))    
@@ -97,10 +97,25 @@ class Plugin(object):
         
         self.window.show()
     
+    def hide_shortcuts_window(self):
+        self.save_shortcuts()
+        self.window.hide()
+        
+    def on_key_press_event(self, window, event):
+        if event.keyval == keysyms.Escape:
+            self.hide_shortcuts_window()
+            return True
+    
+        return False
+        
+    def on_delete_event(self, *args):
+        self.hide_shortcuts_window()
+        return True
+
     def load(self):
         if not Plugin.already_loaded:
             Plugin.already_loaded = True
-
+            self.load_shortcuts()
             self.replace_existing_triggers()
             self.patch_trigger_manager()
 
@@ -111,13 +126,45 @@ class Plugin(object):
         iter = self.actions.get_iter(path)
         accel = accelerator_name(accel_key, accel_mods)
         name = self.actions.get_value(iter, 0)
-        old_accel = self.actions.get_value(iter, 1)
         
-        print name, accel
         shortcuts[name] = accel
         
         self.actions.set_value(iter, 1, accel)
         self.actions.set_value(iter, 4, WEIGHT_BOLD)
         
-        replace_accelerator(old_accel, accel)
+        replace_accelerator(name, accel, False)
+    
+    def on_accel_cleared(self, renderer, path, *args):
+        iter = self.actions.get_iter(path)
+        name = self.actions.get_value(iter, 0)
         
+        shortcuts[name] = None
+        
+        self.actions.set_value(iter, 1, None)
+        self.actions.set_value(iter, 4, WEIGHT_BOLD)
+        
+        replace_accelerator(name, None, False)
+
+    def get_db(self, mode='r'):
+        return open_database(os.path.join("PluginPreferences", "Shortcuts", "shortcuts.gdb"), mode)
+    
+    def load_shortcuts(self):
+        try:
+            db = self.get_db()
+            shortcuts.clear()
+            for k, v in db.iteritems():
+                shortcuts[k] = v
+        except Exception, e:
+            print e
+        finally:
+            db.close()
+
+    def save_shortcuts(self):
+        try:
+            db = self.get_db('n')
+            for k, v in shortcuts.iteritems():
+                db[k] = v
+        except Exception, e:
+            print e
+        finally:
+            db.close()
